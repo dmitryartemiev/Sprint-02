4476ec6610cda745b381901c0ca589fcc7b89415
Dmitry Artemiev <dima77261@gmail.com>
2020-09-30 15:00:21 +0300

    Task 05 complete

--- t05/js/script.js
+++ t05/js/script.js
@@ -1,3 +1,9 @@
-str1 = "You're catnip to a girl like me. Handsome, dazed, and to die for...\n"
-var x = 2
-alert (str1 + 'Length: ' + str1.length + '\nCharacter number' + x + 'is: ' + str1.index)
+str1 = 'You\'re catnip to a girl like me. Handsome, dazed, and to die for...'
+
+alert (str1 + 
+'\nLength: ' + str1.length + 
+'\nCharacter number ' + str1.indexOf('u') + ' is: ' + str1.charAt(2) +
+'\nTo uppercase ' + str1.toUpperCase() +
+'\nConcatenation in a phrase: ' + str1.concat(' - Catwoman') +
+'\n[Batman Returns] ' + (str2 = 'Batman: "I tried to save you."') +
+'\n' + (str3 = 'Selina Kyle: catwoman"Mmm seemsCatwomanlike everyCatWomanwoman you try to savewindsCatWOMANup dead... or deeply resentful.'.replace(/catwoman/ig, ' ')));
