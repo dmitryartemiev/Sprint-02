73d944b59341fe37dfc44c74269e7b473ff0a119
Dmitry Artemiev <dima77261@gmail.com>
2020-10-01 18:32:21 +0300

    t11 (while-do while) is done

---
+++ t11/js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let number = prompt ('Enter number of your choice');
+alert ('The result is in console! Press OK')
+let i = 1;
+while (i<=9) {
+    console.log(number+' * '+i+' = '+number*i+"\n");
+    i++;
+}
